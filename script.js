8b402ee3007fb7e6b1a8e420220bba843979c1d5
Snegha R <sneghakumar0315@gmail.com>
2025-03-15 23:35:44 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,11 @@
 function highlight() {
     //Write your code here
+ const boldWords = document.querySelectorAll('strong');
 
+    // Loop through each <strong> element and change the color to green
+    boldWords.forEach(function(boldWord) {
+        boldWord.style.color = 'rgb(0, 128, 0)';
+    });
 
 }
 
@@ -8,5 +13,10 @@ function highlight() {
 function return_normal() {
     //Write your code here
 
-    
+     const boldWords = document.querySelectorAll('strong');
+
+    // Loop through each <strong> element and change the color to black
+    boldWords.forEach(function(boldWord) {
+        boldWord.style.color = 'rgb(0, 0, 0)';
+    });
 }
